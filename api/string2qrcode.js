6bdd418fea73aae92c417ca42774b8979d85d675
hao.xie <hao.xie@hzztai.com>
2018-09-18 16:18:02 +0800

    string2qrcode

--- api/string2qrcode.js
+++ api/string2qrcode.js
@@ -7,11 +7,22 @@ const app = express();
 
 app.post('/string2qrcode',(req,res) =>{
     let reqString = req.body.stringVal;
-    console.log(req)
-    console.log(reqString);
-    QRCode.toDataURL(reqString, function(err, url) {
-        res.send({'imgUrl':url});
+    let opts = {
+        errorCorrectionLevel:'H',
+        width: req.body.codeSize,
+        color: {
+            dark: req.body.darkColor?req.body.darkColor:'000000ff', // 暗模块颜色
+            light: req.body.lightColor?req.body.lightColor:'ffffffff', // 亮模块颜色
+        }
+    }
+    QRCode.toDataURL(reqString, opts, function(err, url) {
+        if(err) throw err;
+        res.send({imgUrl:url});
     })
+    /*QRCode.toFile('../qrcode.jpg', reqString, opts, function (err) {
+        if (err) throw err;
+        console.log('done');
+    })*/
 })
 
 module.exports = app;
